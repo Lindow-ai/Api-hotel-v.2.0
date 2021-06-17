0f9b471c8de67105e6b74d2292115abf3db52fed
Rémy Le Foll <remy.le-foll@universite-paris-saclay.fr>
2021-06-18 00:56:49 +0200

    create a modern mern app

---
+++ routes/routes.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import { catchErrors } from '../helpers.js'
+import {
+  getRooms,
+  getRoom,
+  addRoom,
+  deleteRoom,
+  updateRoom
+} from '../controllers/roomControllers.js'
+
+// Path avec ES module
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const router = express.Router()
+
+router.get('/api/rooms', catchErrors(getRooms))
+
+router.get('/api/rooms/:id', catchErrors(getRoom))
+
+router.post('/api/rooms', catchErrors(addRoom))
+
+router.patch('/api/rooms/:id', catchErrors(updateRoom))
+
+router.delete('/api/rooms/:id', catchErrors(deleteRoom))
+
+export default router
---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import routes from './routes/routes.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+
+mongoose.connect(process.env.MONGODB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+})
+
+app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`)
+})
